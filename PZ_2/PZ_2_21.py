bbcf8be7f37d952b5c792831fe420102c214c74b
FrainyGGVP <113284520+frainyggvp@users.noreply.github.com>
2023-10-03 13:47:37 +0300

    Update PZ_2_21.py

--- PZ_2/PZ_2_21.py
+++ PZ_2/PZ_2_21.py
@@ -1,3 +1,11 @@
-inp = input("Введите трёхзначное число: ")
-if int(inp).isdigit() and len(str(inp)) == 2:
-  print(str(inp[0] + inp[2] + inp[1]))
+def take_number():
+  inp = input("Введите трёхзначное число: ")
+  if int(inp).isdigit() and len(str(inp)) == 2:
+    print(str(inp[0] + inp[2] + inp[1]))
+  else:
+    print("Некорректные вводные данные")
+    take_number()
+
+if __name__ == "__main__":
+  take_number()
+
