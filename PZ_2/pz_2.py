1236cf63372a385d3aca204fc75d0a97d6ff718e
frainyggvp <galante1925@yandex.ru>
2023-10-14 17:54:59 +0300

    task

---
+++ PZ_2/pz_2.py
@@ -0,0 +1,13 @@
+# Дано трехзначное число. Вывести число, полученное при перестановке цифр десятков и единиц исходного числа
+
+def take_number():
+  inp = input("Введите трёхзначное число: ")
+  if inp.isdigit() and len(inp) == 3 and int(inp) > 99:
+    inp = int(inp)
+    print(str(inp//100) + str(inp%10) + str(inp//10%10))
+  else:
+    print("Некорректные вводные данные")
+    take_number()
+
+if __name__ == "__main__":
+  take_number()
