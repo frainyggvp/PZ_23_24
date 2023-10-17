d90c9d668dc227f94cb9c7d23fe373c5cbdea4a2
frainyggvp <galante1925@yandex.ru>
2023-10-17 19:19:49 +0300

    ready

---
+++ PZ_3/PZ_3_1.py
@@ -0,0 +1,14 @@
+# Вариант 18
+# Дано цело положительное число. Проверить истинность высказывания: "Данное число является нечетным трёхзначным"
+
+integer = input("Введите целое положительное число: ")
+
+if not integer.isdigit():
+    print("Не число! False")
+elif len(integer) != 3:
+    print("Не трёхзначное число! False")
+elif integer.isdigit():
+    if int(integer) % 2 == 0:
+        print("Число чётное! False")
+    else:
+        print("True")
---
+++ PZ_3/PZ_3_2.py
@@ -0,0 +1,16 @@
+# Вариант 18
+# Даны три числа. Найти среднее из них (т.е. число, расположенное между наименьшим и наибольшим)
+
+a = input("Введите число a: ")
+b = input("Введите число b: ")
+c = input("Введите число c: ")
+
+if a.isdigit() and b.isdigit() and c.isdigit():
+    if a > b > c:
+        print(b)
+    elif b > c > a:
+        print(c)
+    elif c > a > b:
+        print(a)
+else:
+    print("Не числа")
