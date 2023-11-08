2ae9a708c5e36e1f555905c4b9aad0fb33656508
frainyggvp <frainyggvp@gmail.com>
2023-11-08 22:51:07 +0300

    Finally

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -5,12 +5,10 @@ a = input("Введите число a: ")
 b = input("Введите число b: ")
 c = input("Введите число c: ")
 
-if a.isdigit() and b.isdigit() and c.isdigit():
-    if a > b > c:
-        print(b)
-    elif b > c > a:
-        print(c)
-    elif c > a > b:
-        print(a)
-else:
-    print("Не все переменные являются числами!")
+try:
+    a, b, c = int(a), int(b), int(c)
+    arr = [a, b, c]
+    arr.sort()
+    print(arr[1])
+except ValueError:
+    print("Некорректный ввод")
