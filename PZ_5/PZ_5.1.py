937ecd919b84edd59f558b272af5a63faceb1902
frainyggvp <frainyggvp@gmail.com>
2023-11-25 16:59:06 +0300

    Finally

---
+++ PZ_5/PZ_5.1.py
@@ -0,0 +1,19 @@
+# Составить программу, в которой функция генерирует четырехзначное число и определяет, есть ли в числе одинаковые цифры
+
+import random
+
+def random_four():
+    random_digit = str(random.randint(999, 10000))
+    journal = []
+    print(random_digit)
+
+    for digit in random_digit:
+        counter = 0
+        for i in random_digit:
+            if digit == i:
+                counter += 1
+        if counter > 1 and digit not in journal:
+            journal.append(digit)
+            print(digit)
+
+random_four()
