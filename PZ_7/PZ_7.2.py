0e99090e6eec4dd8563dd98b277f9ec481d22274
frainyggvp <frainyggvp@gmail.com>
2023-12-09 17:25:25 +0300

    Rabotaem ne spim

--- PZ_7/PZ_7.2.py
+++ PZ_7/PZ_7.2.py
@@ -3,7 +3,16 @@
 пробелом и расположенные в обратном порядке."""
 
 string = "Выйду я в  поле с  конём"
-string_join = ''.join(string.split(' '))
-result = string_join[::-1]
+strings = string.split(' ')
+
+for i in strings:
+    if i == '':
+        strings.remove(i)
+
+strings = strings[::-1]
+string_join = ' '.join(strings)
+result = ""
+for i in strings:
+    result += i + ' '
 
 print(f"Обычная строка: {string}\nИзменённая строка: {result}")
