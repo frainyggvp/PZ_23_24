ee12578e1ed73c9f43caf1ec9dbab50b3ba0a85b
frainyggvp <frainyggvp@gmail.com>
2023-12-16 16:27:14 +0300

    2024!!!

--- PZ_7/PZ_7.2.py
+++ PZ_7/PZ_7.2.py
@@ -2,17 +2,18 @@
 несколькими). Вывести строку, содержащую эти же слова, разделенные одним
 пробелом и расположенные в обратном порядке."""
 
-string = "Выйду я в  поле с  конём"
+string = "Выйду     я в  поле с   конём"
 strings = string.split(' ')
 
 for i in strings:
-    if i == '':
+    if i == '' or i == ' ':
         strings.remove(i)
 
 strings = strings[::-1]
 string_join = ''.join(strings)
 result = ""
 for i in strings:
-    result += i + ' '
+    if i != '':
+        result += i + ' '
 
 print(f"Обычная строка: {string}\nИзменённая строка: {result}")
