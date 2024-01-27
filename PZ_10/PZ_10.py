113cb50bffba2ed2b0732f234280fb26180546ab
frainyggvp <frainyggvp@gmail.com>
2024-01-27 16:08:10 +0300

    фух

--- PZ_10/PZ_10.py
+++ PZ_10/PZ_10.py
@@ -3,20 +3,16 @@
 составить список из множества побывавших у него в гостях друзей, причем хозяина в этот
 список не включать)."""
 
-students_friends = {
-    'Алиса': {'Саша', 'Данил'},
-    'Саша': {'Алиса', 'Аня'},
-    'Данил': {'Алиса', 'Саша'},
-    'Давид': {'Саша', 'Аня'},
-    'Аня': {'Данил', 'Саша'}
-}
+alisa = {'Саша', 'Данил'}
+sasha = {'Алиса', 'Аня'}
+danil = {'Алиса', 'Саша', 'Давид'}
+david = {'Саша'}
 
-guests = {student: set() for student in students_friends}
-
-for student, friends in students_friends.items():
-    for friend in friends:
-        guests[friend].add(student)
-
-for student, friends in guests.items():
-    if friends == set(students_friends.keys()) - {student}:
-        print(f"Студент {student} побывал в гостях у всех остальных студентов.")
+if len(sasha & danil & david) > 0:
+    print("Алиса была в гостях у всех")
+if len(alisa & danil & david) > 0:
+    print("Саша был в гостях у всех")
+if len(alisa & sasha & david) > 0:
+    print("Данил был в гостях у всех")
+if len(alisa & sasha & danil) > 0:
+    print("Давид была в гостях у всех")
