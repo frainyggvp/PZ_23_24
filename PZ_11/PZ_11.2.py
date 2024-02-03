2a84f6bc4663123e4b110f833e5f66d382eaf21f
frainyggvp <frainyggvp@gmail.com>
2024-02-03 12:01:26 +0300

    it was a good day

--- PZ_11/PZ_11.2.py
+++ PZ_11/PZ_11.2.py
@@ -1,15 +1,17 @@
 """Из предложенного текстового файла (text18-18.txt) вывести на экран его содержимое,
 количество знаков пунктуации в первых четырёх строках. Сформировать новый файл, в
 который поместить текст в стихотворной форме выведя строки в обратном порядке."""
+punctuation_marks = [",", ".", "-", "…", "—", ":", ";"]
+signs = 0
 
 with open("text18-18.txt", "r", encoding="utf-16") as file:
     content = file.read()
 with open("text18-18.txt", "r", encoding="utf-16") as file:
     lines = file.readlines()
-signs = 0
+
 for i in range(4):
     for k in lines[i]:
-        if k == "." or k == "," or k == "…":
+        if k in punctuation_marks:
             signs += 1
 print(content, "\nКол-во знаков пунктуации в первых 4 строках:", signs)
 with open("3.txt", "w", encoding="utf-8") as file:
